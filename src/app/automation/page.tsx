f81a6f8812b674d6dd1cc29dc0151b9dc291c022
Aryan <yaryan3087@gmail.com>
2023-10-03 22:42:25 +0530

    automation page

--- src/app/automation/page.tsx
+++ src/app/automation/page.tsx
@@ -25,19 +25,6 @@ export default function Page() {
                 <div className="bg-transparent">
                     <div className="mx-auto max-w-7xl py-14 sm:px-6 sm:py-24 lg:px-8">
                         <div className="relative isolate overflow-hidden bg-white px-6 pt-16 shadow-2xl rounded-lg sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-                            <svg
-                                viewBox="0 0 1024 1024"
-                                className="absolute left-1/4 top-0 -z-10 h-[20rem] w-[20rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/4 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
-                                aria-hidden="true"
-                            >
-                                <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
-                                <defs>
-                                    <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-                                        <stop stopColor="#7775D6" />
-                                        <stop offset={1} stopColor="#E935C1" />
-                                    </radialGradient>
-                                </defs>
-                            </svg>
                             <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
                                 <h2 className="text-xl font-bold tracking-tight text-slate-800 sm:text-4xl">
                                     Achieve Your Vision
